6eb79f277d71aae2e219ca508365a61491f9ca30
Hayden Bradfield <hbradfield20@gmail.com>
2021-03-13 20:46:12 -0500

    added markup for home and added styles and assets

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const config = require('./config');
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.set('view engine','ejs');
 app.set('views', './views');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', require('./routes/admin'));
 app.use(require('./routes/general'));
